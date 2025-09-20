11f2410e0d568056d600ceb7c093e6c70ee816f7
eduardovitor <eduardovcn74@gmail.com>
2025-09-20 15:01:34 -0300

    Creating new files of frontend pages for new terms, about team and updating the routes of backend

--- app.py
+++ app.py
@@ -8,4 +8,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/glossario')
+def glossario():
+    return render_template('glossario.html')
+
+@app.route('/novo_termo')
+def novo_termo():
+    return render_template('novo_termo.html')
+
 app.run()
